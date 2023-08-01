72103176cb5b6c1dacfccf405891943c6db5919c
zaeemali272 <zaeemali272@gmail.com>
2023-08-01 17:17:26 +0500

    mobile menu bar added

--- Contact/app.js
+++ Contact/app.js
@@ -24,16 +24,17 @@ $('.fa-bars').hover(function () {
 
 // Get the button:
 let mybutton = document.getElementById("myBtn");
+const tablet = window.matchMedia('(min-width: 420px)')
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 && tablet.matches) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
 }
 
 // When the user clicks on the button, scroll to the top of the document
--- app.js
+++ app.js
@@ -24,17 +24,20 @@ $('.fa-bars').hover(function () {
 
 // Get the button:
 let mybutton = document.getElementById("myBtn");
+const tablet = window.matchMedia('(min-width: 420px)')
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 && tablet.matches) {
         mybutton.style.display = "block";
     } else {
         mybutton.style.display = "none";
     }
 }
+
+
 
 // When the user clicks on the button, scroll to the top of the document
 // function topFunction() {
